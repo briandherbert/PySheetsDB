c24a44fea23b8154d4f48a3640239e3022dcc759
Brian Herbert <brian.herbert@life.church>
2023-10-16 17:13:31 -0500

    Specify lib file in setup

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="PySheetsDB",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
+    py_modules=["py_sheets_db"],
     install_requires=[
         'google-api-python-client',
         # ... any other dependencies ...
